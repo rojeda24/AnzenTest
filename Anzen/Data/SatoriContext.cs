c6e774723f82afeb224c2e941413bce0f2eedc08
Raul Ojeda <raul.ojeda.luna@gmail.com>
2023-12-30 16:01:41 -0700

    Initial Commit including a submission model, and an Initial Migration creating the submission table using an ORM

---
+++ Anzen/Data/SatoriContext.cs
@@ -0,0 +1,20 @@
+﻿using Anzen.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anzen.Data
+{
+    public class SatoriContext : DbContext
+    {
+        public SatoriContext(DbContextOptions<SatoriContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Submission> Submission { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=satori.db");//Security and scalability outside the scope 
+        }
+    }
+}
---
+++ Anzen/Models/Submission.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Anzen.Models
+{
+    public class Submission
+    {
+        [Key]
+        public int Id {get; set; }
+        [Required]
+        public string AccountName { get; set; } = null!;
+    }
+}
