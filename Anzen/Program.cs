71bc6709d436aa423303e06ef5ae0d0b976f3cf1
Raul Ojeda <raul.ojeda.luna@gmail.com>
2023-12-30 18:28:48 -0700

    page and pageSize are now query parameters

--- Anzen/Program.cs
+++ Anzen/Program.cs
@@ -1,5 +1,4 @@
 using Anzen.Data;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,7 +19,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/submissions/{page:int?}/{pageSize:int?}", async (SatoriContext context, int page = 1, int pageSize = 10) =>
+
+app.MapGet("/submissions", async (SatoriContext context, int page = 1, int pageSize = 10) =>
 {
     var submissions = await context.Submission
     .Skip((page - 1) * pageSize)
