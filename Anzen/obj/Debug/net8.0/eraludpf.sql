8b9c03c3826b80d147e601117888e11a580c0c5e
Raul Ojeda <raul.ojeda.luna@gmail.com>
2023-12-31 22:22:35 -0700

    removed .gitignore and sharing EVERYTHINGgit status

---
+++ Anzen/obj/Debug/net8.0/eraludpf.sql
@@ -0,0 +1,34 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" TEXT NOT NULL CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY,
+    "ProductVersion" TEXT NOT NULL
+);
+
+BEGIN TRANSACTION;
+
+CREATE TABLE "Submission" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_Submission" PRIMARY KEY AUTOINCREMENT,
+    "AccountName" TEXT NOT NULL
+);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20231230070750_InitialCreate', '8.0.0');
+
+COMMIT;
+
+BEGIN TRANSACTION;
+
+ALTER TABLE "Submission" ADD "EffectiveDate" TEXT NOT NULL DEFAULT '0001-01-01';
+
+ALTER TABLE "Submission" ADD "ExpirationDate" TEXT NOT NULL DEFAULT '0001-01-01';
+
+ALTER TABLE "Submission" ADD "Premium" TEXT NOT NULL DEFAULT '0.0';
+
+ALTER TABLE "Submission" ADD "Sic" TEXT NOT NULL DEFAULT '';
+
+ALTER TABLE "Submission" ADD "UwName" TEXT NOT NULL DEFAULT '';
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20231231025741_SubmissionMoreFields', '8.0.0');
+
+COMMIT;
+
